c7d87fb7e4faa6d1c60732115fd3603e50cdb0a7
dacong <2540740154@qq.com>
2017-12-20 21:55:39 +0800

    add regist-button

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,20 +13,16 @@ window.onload = function() {
       return;
     }
 
-    if (validUsername(username) && validID(id) && 
-       validPhone(phone) && validMail(mail)) {
+    if (validUsername(username) && validID(passwd)) {
       $.ajax({
         url: "/",
         method: "POST",
         data: {
           username: $('#username').val(),
-          id: $('#studentID').val(),
-          phone: $('#phone').val(),
-          mail: $('#mail').val()
+          passwd: $('#passwd').val(),
         },
         success: function(data, textStatus, jqXHR) { 
-          if (data == "注册成功") {
-            alert(data+", 点击跳转到详情页");
+          if (data == "登录成功") {
             window.location.href = "?username="+$("#username").val();
           }
           else {
@@ -40,15 +36,33 @@ window.onload = function() {
 
 function validUsername(username) {
   if (username.length < 6 || username.length > 18) {
-    TIP("注册失败， 用户名必须由6~18位英文字母、数字或下划线组成");
+    TIP("用户名由6~18位英文字母、数字或下划线组成");
     return false;
   } else if (!/[a-z|A-Z]/.test(username[0])) {
-    TIP("注册失败， 用户名必须以英文字母开头");
+    TIP("用户名以英文字母开头");
     return false;
   } else {
     for (var i = 1; i < username.length; i++) {
       if (!/[a-z|A-Z|_|0-9]/.test(username[i])) {
-        TIP("注册失败， 用户名必须由英文字母、数字或下划线组成");
+        TIP("用户名只由英文字母、数字或下划线组成");
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function validPasswd(passwd) {
+  if (username.length < 6 || username.length > 18) {
+    TIP("用户名由6~18位英文字母、数字或下划线组成");
+    return false;
+  } else if (!/[a-z|A-Z]/.test(username[0])) {
+    TIP("用户名以英文字母开头");
+    return false;
+  } else {
+    for (var i = 1; i < username.length; i++) {
+      if (!/[a-z|A-Z|_|0-9]/.test(username[i])) {
+        TIP("用户名只由英文字母、数字或下划线组成");
         return false;
       }
     }
