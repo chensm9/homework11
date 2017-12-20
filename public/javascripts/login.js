1ad00d98b32fb22c8260f4105f5b4b64eb8513ba
dacong <2540740154@qq.com>
2017-12-20 22:05:42 +0800

    完成login.js

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,19 +1,21 @@
 window.onload = function() {
+  $('#registbutton').click(function(){
+    window.location.href = '/regist';
+  })
 
-  $('button').click(function() {
+  $('#loginbutton').click(function() {
     var username = $("#username").val();
-    var passwd = $('passwd').val();
+    var passwd = $('#passwd').val();
 
     if (username == "") {
       TIP("请填写用户名");
       return;
-    }
-    else if (passwd == "") {
-      TIP("请填写用户名");
+    } else if (passwd == "") {
+      TIP("请填写密码");
       return;
-    }
-
-    if (validUsername(username) && validID(passwd)) {
+    } else {
+      alert($('#username').val());
+      alert($('#passwd').val());
       $.ajax({
         url: "/",
         method: "POST",
@@ -32,42 +34,6 @@ window.onload = function() {
       });
     }
   });
-}
-
-function validUsername(username) {
-  if (username.length < 6 || username.length > 18) {
-    TIP("用户名由6~18位英文字母、数字或下划线组成");
-    return false;
-  } else if (!/[a-z|A-Z]/.test(username[0])) {
-    TIP("用户名以英文字母开头");
-    return false;
-  } else {
-    for (var i = 1; i < username.length; i++) {
-      if (!/[a-z|A-Z|_|0-9]/.test(username[i])) {
-        TIP("用户名只由英文字母、数字或下划线组成");
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-function validPasswd(passwd) {
-  if (username.length < 6 || username.length > 18) {
-    TIP("用户名由6~18位英文字母、数字或下划线组成");
-    return false;
-  } else if (!/[a-z|A-Z]/.test(username[0])) {
-    TIP("用户名以英文字母开头");
-    return false;
-  } else {
-    for (var i = 1; i < username.length; i++) {
-      if (!/[a-z|A-Z|_|0-9]/.test(username[i])) {
-        TIP("用户名只由英文字母、数字或下划线组成");
-        return false;
-      }
-    }
-  }
-  return true;
 }
 
 var timeout;
